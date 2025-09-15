b65da0e2509464a4a8271a00040f6852f14fd4f3
Diego0Alves <anibodyelse1@gmail.com>
2025-09-15 20:29:20 -0300

    teste0

--- test_app.py
+++ test_app.py
@@ -4,3 +4,8 @@ def test_hello_world():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert response.data == b'Ol\xc3\xa1, Mundo!'
+
+def healthcheck():
+    response = app.test_client().get('/healthcheck')
+    assert response.status_code == 200
+    assert response.data == b'{"status": "ok"}'
