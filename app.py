647c9eb6158214f5a77ab4552b8dfbffd15617b2
Diego0Alves <anibodyelse1@gmail.com>
2025-09-15 20:23:31 -0300

    app.py up

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Olá, Mundo!"
+@app.route("/healthcheck")
+def health():
+    return '{"status": "ok"}'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
